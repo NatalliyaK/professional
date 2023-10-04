ef5b6b9e98b4951a59a6b451bae359165be33a7b
Natallia <natasupernice@gmail.com>
2023-10-04 17:26:05 +0300

    add: section tab desctop

--- src/assets/js/swiper.js
+++ src/assets/js/swiper.js
@@ -3,6 +3,23 @@ const curseSlider = new Swiper('.curse__swiper', {
     slidesPerView: 3,
     spaceBetween: 80,
     speed: 800,
+
+    breakpoints: {
+        // when window width is >= 320px
+        320: {
+            slidesPerView: 1,
+        },
+        // when window width is >= 480px
+        480: {
+            slidesPerView: 2,
+        },
+        // when window width is >= 640px
+        640: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+        }
+    },
+
     navigation: {
         nextEl: '.btn-prev',
         prevEl: '.btn-next',
